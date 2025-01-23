3a3e841ba4eae3318cf0c5f3d6103a5d8c753e61
Myster7494 <jackychiu0101@gmail.com>
2025-01-23 23:35:17 +0800

    feat: modify syllabus

--- app/page.tsx
+++ app/page.tsx
@@ -51,9 +51,9 @@ export default function Page() {
                         <p>2. Java: 基礎語法</p>
                         <p>3. Java: 基礎語法 + IntelliJ IDEA + Java: 流程控制</p>
                         <p>4. Java: 流程控制</p>
-                        <p>5. Java: 流程控制 + 靜態成員</p>
-                        <p>6. Java: 靜態成員 + 類別與物件</p>
-                        <p>7. Java: 類別與物件 + 套件 + Kahoot 大賽</p>
+                        <p>5. Java: 流程控制</p>
+                        <p>6. Java: 流程控制 + 陣列</p>
+                        <p>7. Kahoot 大賽</p>
                     </div>
                     <div style={{
                         border: '1px solid #eaeaea',
@@ -62,13 +62,12 @@ export default function Page() {
                         borderRadius: '25px'
                     }}>
                         <h2 style={{margin: 0}}>下學期：</h2>
-                        <p>1. 空值與參考 + 包裝類別與工具類別(1)</p>
-                        <p>2. Java: 介面</p>
-                        <p>3. Java: 泛型</p>
-                        <p>4. Java: 工具類別(2)</p>
-                        <p>5. Java: 工具類別(2)</p>
-                        <p>6. Java: 例外處理 + Git + Github + Java 專案前置作業</p>
-                        <p>7. Java 專案製作</p>
+                        <p>1. 靜態成員 + 類別與物件</p>
+                        <p>2. Java: 類別與物件</p>
+                        <p>3. Java: 套件與存取修飾子 + 包裝類別與工具類別(1) + 空值與參考 + 介面</p>
+                        <p>4. Java: 介面 + 泛型 + 工具類別(2) + 資料結構</p>
+                        <p>5. Kahoot 大賽</p>
+                        <p>（註：剩下的教不完）</p>
                     </div>
                 </div>
                 <hr style={{width: "100%"}}/>
@@ -84,7 +83,7 @@ export default function Page() {
                     <p style={{textIndent: '2rem', margin: "0.5rem"}}>
                         {
                             "    在社內，我們營造輕鬆愉快的氛圍，希望大家在互相學習的過程中能享受到學習的樂趣。我們會根據社員的反饋和需求調整教學方向，並提供學長親自輔導的機會，讓學習更有效率。" +
-                            "加入我們的社團，不僅能獲得程式語言學習的指導和同伴的支持，也能在準備APCS考試時共同討論、解決難題。無論是追求程式設計技能還是準備考試，我們都歡迎你的加入！如有興趣，請隨時私訊我們詢問更多詳情。(╯✧∇✧)╯"
+                            "加入我們的社團，不僅能獲得程式語言學習的指導和同伴的支持，也能在準備 APCS 考試時共同討論、解決難題。無論是追求程式設計技能還是準備考試，我們都歡迎你的加入！如有興趣，請隨時私訊我們詢問更多詳情。(╯✧∇✧)╯"
                         }
                     </p>
                 </div>
